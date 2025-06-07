03ce4b74b70fed0c5f5ed24958679678c8b06e1f
Martin Manrique <75140194+M117n@users.noreply.github.com>
2025-06-07 13:16:28 -0600

    test: expand cn utility cases

--- tests/unit/utils.unit.test.ts
+++ tests/unit/utils.unit.test.ts
@@ -8,4 +8,16 @@ describe('cn', () => {
   it('merges conflicting class names, using the last one', () => {
     expect(cn('p-4', 'p-2')).toBe('p-2');
   });
+
+  it('handles conditional object values', () => {
+    expect(cn('foo', { bar: true, baz: false })).toBe('foo bar');
+  });
+
+  it('ignores undefined and null values', () => {
+    expect(cn('foo', undefined, null, 'bar')).toBe('foo bar');
+  });
+
+  it('merges arrays of class names', () => {
+    expect(cn(['foo', null], ['bar'], { baz: true })).toBe('foo bar baz');
+  });
 });
